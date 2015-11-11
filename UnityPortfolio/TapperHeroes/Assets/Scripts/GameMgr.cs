28664bf09939428efa4454678301e075adede202
gun265 <chan8709@naver.com>
2015-11-11 16:34:39 +0900

    TapperHeroes 빠진 파일 추가

---
+++ UnityPortfolio/TapperHeroes/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class GameMgr : MonoBehaviour
+{
+    public GameObject Player = null;
+    public Transform Monster = null;
+    public List<GameObject> TextList = null;
+    public bool OnDebuging = false;
+    public bool IsGamePlaying = true;    
+    public uint WeaponLevel = 1;
+    public uint Damage = 1;
+    public float AttackTimer = 30.0f;
+    public float CriticalPercent = 0;
+
+    uint StageNumber = 1;
+    uint lastStageNumber = 0;
+    uint Gold = 0;
+    uint ActivatedHeroCount = 0;
+    float DamageRand = 0;
+    float CurrentTimer = 0;
+    
+
+    void Awake()
+    {
+        DamageRand = Damage * 0.05f;
+        ResetTimer();
+        if (Player == null)
+        {
+            Player = GameObject.Find("FreeCharacter_model");
+        }
+        if (TextList.Count == 0)
+        {
+            GameObject Temp = GameObject.Find("UI Root/UICam/Panel/DMGText");
+            int ChildCount = Temp.transform.childCount;
+            for( int i = 0; i < ChildCount; i++)
+            {
+                TextList.Add(Temp.transform.GetChild(i).gameObject);
+            }
+        }
+        if( Monster == null)
+        {
+            Monster = GameObject.Find("Monster").transform;
+        }
+    }
+
+    public void PlayEffect(Vector3 _Position, string _EffectName)
+    {
+        string Path = string.Concat("PreFab/Effect/", _EffectName);
+
+        GameObject Temp = NGUITools.AddChild(gameObject, (GameObject)Resources.Load(Path));
+        Temp.transform.position = UICamera.currentCamera.ScreenToWorldPoint(_Position);
+    }
+
+    void LoadData()
+    {
+
+    }
+
+    void SaveData()
+    {
+
+    }
+
+    void OnApplicationQuit()
+    {
+
+    }
+
+    void OnApplicationPause()
+    {
+        IsGamePlaying = true;
+    }
+
+    public void Attack()
+    {
+        uint CurrntDamage = Damage + (uint)DamageRand;
+        if( Random.Range(0.0f, 100.0f) <= CriticalPercent)
+        {
+            CurrntDamage = (uint)(CurrentTimer * 1.5f);
+        }
+
+        DamagePrint(CurrntDamage);
+    }
+
+    void DamagePrint(uint _Damage)
+    {
+        foreach(GameObject _object in TextList)
+        {
+            if(!_object.active)
+            {
+                _object.SetActive(true);
+                _object.GetComponent<DamageText>().Init(_Damage, Monster.position);
+                return;
+            }
+        }
+    }
+
+    void ChangeEnvironment()
+    {
+
+    }
+
+    void ResetTimer()
+    {
+        CurrentTimer = AttackTimer;
+    }
+}
---
+++ UnityPortfolio/TapperHeroes/Assets/Scripts/Tapping.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tapping : MonoBehaviour {
+    public GameMgr Manager = null;
+    public bool CanIHit = true;
+    //int a = 0;
+    float time = 0;
+
+    void Awake()
+    {
+        if( Manager == null)
+        {
+            Manager = GameObject.Find("GameManager").GetComponent<GameMgr>();
+            if( Manager == null)
+            {
+                Debug.Log("Can not find Manager in Tapping.cs");
+            }
+        }
+    }
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+	void OnPress(bool _IsPress)
+    {
+        if (CanIHit  && _IsPress)
+        {
+            CanIHit = false;
+            Vector3 position = new Vector3(UICamera.lastTouchPosition.x, UICamera.lastTouchPosition.y, 0);
+            Manager.PlayEffect(position, "Hit");
+            //a++;            
+            //Debug.Log("hit : " + a + "   time : " + time.ToString("#.#0"));
+            Manager.Attack();
+        }
+
+        else if(!_IsPress)
+        {
+            CanIHit = true;
+        }
+    }
+}
