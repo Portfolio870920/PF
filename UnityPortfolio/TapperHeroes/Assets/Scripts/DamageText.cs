d1c40391547ffbf30206805dda007837796b2bd0
gun265 <chan8709@naver.com>
2015-11-16 13:28:42 +0900

    용병 시스템 구현 중  - HeroFoundation 클래스 구현  - HeroFoundation 를 상속받는 영웅클래스를 임의 추가할 수 있도록 구조 구현  - 영웅의 공격 시 선택한 이펙트가 자동으로 몬스터에게 날라갈 수 있도록 구현  - 영웅 공격 이펙트가 몬스터에게 접근 시 데미지를 띄우도록 구조변경이 필요

--- UnityPortfolio/TapperHeroes/Assets/Scripts/DamageText.cs
+++ UnityPortfolio/TapperHeroes/Assets/Scripts/DamageText.cs
@@ -3,38 +3,42 @@ using System.Collections;
 
 public class DamageText : MonoBehaviour
 {
-    public float LifeTime = 30.0f;
+    public float LifeTime = 0.5f;
+    float CurrentLifeTime = 0;
     UILabel Label = null;
-    Camera UIcam = null;
+    Camera UICam = null;
     Vector3 Target = Vector3.zero;
     
-    public void Init(uint _Damage, Vector3 _Target)
+    public void Init(uint _Damage, Vector3 _Target, Color _Color)
     {
         Target = _Target;
         Label = GetComponent<UILabel>();
         Label.text = _Damage.ToString();
-        LifeTime = 30.0f;
-        if ( UIcam == null)
+        Label.color = _Color;
+        CurrentLifeTime = LifeTime;
+        if (UICam == null)
         {
-            Camera guiCam = NGUITools.FindCameraForLayer(gameObject.layer);
+            UICam = NGUITools.FindCameraForLayer(gameObject.layer);
         }
+        Vector3 pos = Camera.main.WorldToScreenPoint(_Target);
+        transform.position = UICam.ScreenToWorldPoint(new Vector3(pos.x, pos.y + 100.0f, 0));
     }
-    
-	void Update ()
-    {
-	    if( (LifeTime -= Time.deltaTime) > 0)
-        {
-            Vector3 pos = UIcam.WorldToScreenPoint(Target);
-            transform.position = UIcam.ScreenToWorldPoint(new Vector3(pos.x, pos.y + 10.0f, 0));
 
+    void Update ()
+    {
+	    if( (CurrentLifeTime -= Time.deltaTime) > 0)
+        {
             Vector3 Position = transform.position;
-            Position.y += 10 * Time.deltaTime;
+            Position.y += Time.deltaTime * 0.5f;
             transform.position = Position;
+
+            Color tempColor = Label.color;
+            tempColor.a -= 255.0f / (Time.deltaTime * 0.5f);
         }
         else
         {
+            Init(0, Target, Color.yellow);
             gameObject.SetActive(false);
-            Init(0,Vector3.zero);
         }
 	}
 }
--- UnityPortfolio/TapperHeroes/Assets/Scripts/Tapping.cs
+++ UnityPortfolio/TapperHeroes/Assets/Scripts/Tapping.cs
@@ -2,37 +2,17 @@
 using System.Collections;
 
 public class Tapping : MonoBehaviour {
-    public GameMgr Manager = null;
     public bool CanIHit = true;
-    //int a = 0;
     float time = 0;
-
-    void Awake()
-    {
-        if( Manager == null)
-        {
-            Manager = GameObject.Find("GameManager").GetComponent<GameMgr>();
-            if( Manager == null)
-            {
-                Debug.Log("Can not find Manager in Tapping.cs");
-            }
-        }
-    }
-    void Update()
-    {
-        time += Time.deltaTime;
-    }
-
+    
 	void OnPress(bool _IsPress)
     {
         if (CanIHit  && _IsPress)
         {
             CanIHit = false;
             Vector3 position = new Vector3(UICamera.lastTouchPosition.x, UICamera.lastTouchPosition.y, 0);
-            Manager.PlayEffect(position, "Hit");
-            //a++;            
-            //Debug.Log("hit : " + a + "   time : " + time.ToString("#.#0"));
-            Manager.Attack();
+            GameMgr.GetInstance().PlayEffect(position, "Hit");
+            GameMgr.GetInstance().Attack(GameMgr.GetInstance().Damage, Color.yellow);
         }
 
         else if(!_IsPress)
